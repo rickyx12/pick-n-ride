867ac08429aac7e5f0d9ac7bbdb7a74a119fd753
RICARDO A. OSIT JR <ricardo.cloudpanda@gmail.com>
2019-04-12 18:27:23 +0800

    fix payload

--- index.js
+++ index.js
@@ -100,9 +100,9 @@ function handleMessage(sender_psid, received_message) {
 
   let response;
  
-  console.log("xx--"+received_message);
+  // console.log("xx--"+received_message);
 
-  if(received_message.quick_reply.payload == "PASSENGER_YES") {
+  if(received_message.quick_reply.payload != "") {
     
     getPickUpPoint(sender_psid);
   
